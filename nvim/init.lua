030bca2f4463fc1b90a3ecae055815ee47b0b574
j-zunino <zuninojuanbautista@gmail.com>
2024-08-04 00:27:50 -0300

    nvim: decrease scrolloff to 5

--- nvim/init.lua
+++ nvim/init.lua
@@ -46,7 +46,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 5
 
 -- [[ Basic Keymaps ]]
 vim.opt.hlsearch = true
