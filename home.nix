e52e30f5eee0334283132a2d8cb1f6b86fd68c14
j-zunino <zuninojuanbautista@gmail.com>
2024-09-01 20:46:00 -0300

    feat: nest home.nix config

--- home.nix
+++ home.nix
@@ -7,47 +7,47 @@
     ./program/nvim.nix
   ];
 
-  home.username = "juan";
-  home.homeDirectory = "/home/juan";
-
   nixpkgs.config.allowUnfree = true;
 
-  home.packages = with pkgs; [
-    wget
-    curl
-    bat
-    fzf
-    gcc
-    gnumake
-    unzip
-    nodejs_22
-    python312Packages.pip
-    ripgrep
-    python3
-    fastfetch
-    vscode-fhs
-    brave
-    discord
-    gimp
-    inkscape
-    vlc
-  ];
-
-  home.file = {
+  programs = {
+    home-manager.enable = true;
+    bash.enable = true;
+    zsh.enable = true;
   };
 
-  programs.bash.enable = true;
-  programs.zsh.enable = true;
+  home = {
+    username = "juan";
+    homeDirectory = "/home/juan";
 
-  home.sessionPath = [
-    "$HOME/dotfiles/script"
-  ];
+    sessionPath = [
+      "$HOME/dotfiles/script"
+    ];
 
-  home.sessionVariables = {
-    # EDITOR = "emacs";
+    sessionVariables = {
+      # EDITOR = "emacs";
+    };
+
+    stateVersion = "24.05";
+
+    packages = with pkgs; [
+      wget
+      curl
+      bat
+      fzf
+      gcc
+      gnumake
+      unzip
+      nodejs_22
+      python312Packages.pip
+      ripgrep
+      python3
+      fastfetch
+      vscode-fhs
+      brave
+      discord
+      gimp
+      inkscape
+      vlc
+    ];
   };
-
-  programs.home-manager.enable = true;
-
-  home.stateVersion = "24.05";
 }
