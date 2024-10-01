01c4ef059087d1cd53186b6a478deda563b3c8a5
j-zunino <zuninojuanbautista@gmail.com>
2024-10-01 02:00:45 -0300

    feat(nixos): add stylua

--- modules/home/text-editors/nvim/nvim.nix
+++ modules/home/text-editors/nvim/nvim.nix
@@ -1,6 +1,20 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
     programs.neovim.enable = true;
     xdg.configFile.nvim.source = ../nvim;
+
+    home = {
+        packages = with pkgs; [
+            stylua
+        ];
+
+        file."/home/juan/dotfiles/.stylua.toml".text = ''
+            indent_type = "Spaces"
+            indent_width = 4
+            column_width = 80
+            quote_style = "AutoPreferSingle"
+        '';
+
+    };
 }
