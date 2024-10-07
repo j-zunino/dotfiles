71b2fa7bf8337f96cae26ed35950b840799cfce5
j-zunino <zuninojuanbautista@gmail.com>
2024-10-07 01:14:41 -0300

    feat(nixos): add picom

--- modules/nixos/services/services.nix
+++ modules/nixos/services/services.nix
@@ -6,5 +6,11 @@
         printing.enable = true;
         dbus.enable = true;
         #flatpak.enable = true;
+
+        picom = {
+            enable = true;
+            backend = "egl";
+            vSync = "opengl";
+        };
     };
 }
