c514ac7f2ce3c499aeda5e500b45eae7e0d4c75b
j-zunino <zuninojuanbautista@gmail.com>
2024-10-17 22:21:23 -0300

    feat(wezterm): change font size and add force_reverse_video_cursor

--- modules/home/wezterm.nix
+++ modules/home/wezterm.nix
@@ -16,11 +16,12 @@
 
                     -- font = wezterm.font 'CaskaydiaCove Nerd Font'
                     font = wezterm.font 'JetBrainsMono Nerd Font',
+                    font_size = 10.9,
 
                     enable_tab_bar = false,
 
                     hide_mouse_cursor_when_typing = true,
-
+                    force_reverse_video_cursor = true,
                     window_background_opacity = 0.9,
 
                     cursor_blink_rate = 0,
@@ -48,13 +49,13 @@
                         "#232a2e" "#e67e80" "#a7c080" "#dbbc7f" "#7fbbb3" "#d699b6" "#83c092" "#d3c6aa"
                     ];
 
+                    foreground = "#d3c6aa";
                     background = "#2d353b";
+                    cursor_fg = "#2d353b";
                     cursor_bg = "#d3c6aa";
                     cursor_border = "#d3c6aa";
-                    cursor_fg = "#2d353b";
-                    foreground = "#d3c6aa";
+                    selection_fg = "none";
                     selection_bg = "#543a48";
-                    selection_fg = "#d3c6aa";
                 };
             };
         };
