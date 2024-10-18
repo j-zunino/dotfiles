93497239d7af93c4609efd69b04ca4cbbe236f88
j-zunino <zuninojuanbautista@gmail.com>
2024-10-18 01:41:08 -0300

    feat(wezterm): add mux

--- modules/home/wezterm.nix
+++ modules/home/wezterm.nix
@@ -18,7 +18,36 @@
                     font = wezterm.font 'JetBrainsMono Nerd Font',
                     font_size = 10.9,
 
-                    enable_tab_bar = false,
+                    enable_tab_bar = true,
+                    hide_tab_bar_if_only_one_tab = false,
+                    tab_bar_at_bottom = true,
+                    use_fancy_tab_bar = false,
+                    tab_and_split_indices_are_zero_based = false,
+
+                    colors = {
+                        tab_bar = {
+                            background = "none",
+
+                            active_tab = {
+                                bg_color = "#a7c080",
+                                fg_color = "#232a2e",
+                            },
+                            inactive_tab = {
+                                bg_color = "#343f44",
+                                fg_color = "#4f585e",
+                            },
+
+                            new_tab = {
+                                bg_color = "none",
+                                fg_color = "#343f44",
+                            },
+                            new_tab_hover = {
+                                bg_color = "#343f44",
+                                fg_color = "#4f585e",
+                            },
+
+                        },
+                    },
 
                     hide_mouse_cursor_when_typing = true,
                     force_reverse_video_cursor = true,
@@ -35,7 +64,86 @@
                         top = 0,
                         bottom = 0,
                     },
+
+
+                    -- tmux
+                    leader = { mods = "ALT", key = "w", timeout_milliseconds = 1000 },
+                    keys = {
+                        {
+                            mods = "ALT",
+                            key = "Enter",
+                            action = wezterm.action.SpawnTab "CurrentPaneDomain",
+                        },
+                        {
+                            mods = "ALT",
+                            key = "q",
+                            action = wezterm.action.CloseCurrentPane { confirm = true },
+                        },
+
+                        {
+                            mods = "ALT",
+                            key = "{",
+                            action = wezterm.action.ActivateTabRelative(-1),
+                        },
+                        {
+                            mods = "ALT",
+                            key = "}",
+                            action = wezterm.action.ActivateTabRelative(1),
+                        },
+
+
+                        {
+                            mods = "ALT",
+                            key = "s",
+                            action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+                        },
+                        {
+                            mods = "ALT",
+                            key = "v",
+                            action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+                        },
+
+                        {
+                            mods = "ALT",
+                            key = "j",
+                            action = wezterm.action.ActivatePaneDirection "Next",
+                        },
+                        {
+                            mods = "ALT",
+                            key = "k",
+                            action = wezterm.action.ActivatePaneDirection "Prev",
+                        },
+
+                        {
+                            mods = "LEADER",
+                            key = "h",
+                            action = wezterm.action.AdjustPaneSize { "Left", 5 },
+                        },
+                        {
+                            mods = "LEADER",
+                            key = "l",
+                            action = wezterm.action.AdjustPaneSize { "Right", 5 },
+                        },
+                        {
+                            mods = "LEADER",
+                            key = "j",
+                            action = wezterm.action.AdjustPaneSize { "Down", 5 },
+                        },
+                        {
+                            mods = "LEADER",
+                            key = "k",
+                            action = wezterm.action.AdjustPaneSize { "Up", 5 },
+                        },
+                    },
                 }
+
+                for i = 0, 8 do
+                    table.insert(config.keys, {
+                        mods = "ALT",
+                        key = tostring(i + 1),
+                        action = wezterm.action.ActivateTab(i),
+                    })
+                end
 
                 return config
             '';
