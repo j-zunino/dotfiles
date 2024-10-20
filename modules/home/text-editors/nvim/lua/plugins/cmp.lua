4bd0859a9974f9d30c7cea5bb9b858333653a92f
j-zunino <zuninojuanbautista@gmail.com>
2024-10-20 20:18:25 -0300

    feat(nvim): move plugins in init.lua to lua/plugins/

--- modules/home/text-editors/nvim/lua/plugins/cmp.lua
+++ modules/home/text-editors/nvim/lua/plugins/cmp.lua
@@ -11,9 +11,6 @@ return {
                 return "make install_jsregexp"
             end)(),
             dependencies = {
-                -- `friendly-snippets` contains a variety of premade snippets.
-                --    See the README about individual language/framework/plugin snippets:
-                --    https://github.com/rafamadriz/friendly-snippets
                 {
                     'rafamadriz/friendly-snippets',
                     config = function()
@@ -23,7 +20,6 @@ return {
             },
         },
         "saadparwaiz1/cmp_luasnip",
-
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-path",
     },
--- modules/home/text-editors/nvim/lua/plugins/conform.lua
+++ modules/home/text-editors/nvim/lua/plugins/conform.lua
@@ -15,9 +15,6 @@ return {
     opts = {
         notify_on_error = false,
         format_on_save = function(bufnr)
-            -- Disable "format_on_save lsp_fallback" for languages that don't
-            -- have a well standardized coding style. You can add additional
-            -- languages here or re-enable it for the disabled ones.
             local disable_filetypes = { c = true, cpp = true }
 
             return {
@@ -35,12 +32,6 @@ return {
             typescript = { "prettierd", "prettier" },
             json = { "prettierd", "prettier" },
             markdown = { "prettierd", "prettier" },
-
-            -- Conform can also run multiple formatters sequentially
-            -- python = { "isort", "black" },
-            --
-            -- You can use a sub-list to tell conform to run *until* a formatter
-            -- is found.
         },
     },
 }
