98eca666295a38e8f2d8ec01e510126ea9cfb1e8
j-zunino <zuninojuanbautista@gmail.com>
2024-11-24 03:01:23 -0300

    feat(nvim): add diagnostic highlighting

--- modules/home/text-editors/nvim/lua/plugins/theme.lua
+++ modules/home/text-editors/nvim/lua/plugins/theme.lua
@@ -5,7 +5,19 @@ return {
     config = function()
         require('everforest').setup({
             transparent_background_level = 1,
+            -- asdadsa
             disable_italic_comments = true,
+
+            on_highlights = function(hl, palette)
+                hl.DiagnosticVirtualTextError =
+                    { fg = palette.red, bg = palette.bg_red }
+                hl.DiagnosticVirtualTextHint =
+                    { fg = palette.green, bg = palette.bg_green }
+                hl.DiagnosticVirtualTextInfo =
+                    { fg = palette.blue, bg = palette.bg_blue }
+                hl.DiagnosticVirtualTextWarn =
+                    { fg = palette.yellow, bg = palette.bg_yellow }
+            end,
         })
 
         vim.cmd.colorscheme('everforest')
