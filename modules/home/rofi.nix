8aa2afa5919f32b03dbdafd98a29de8b6a2dfa08
j-zunino <zuninojuanbautista@gmail.com>
2025-01-26 20:41:08 -0300

    feat(rofi): change keybinds

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -6,6 +6,14 @@
             package = pkgs.rofi-wayland;
             enable = true;
             location = "center";
+            extraConfig = {
+                kb-row-up = "Up,Control+k";
+                kb-row-down = "Down,Control+j";
+                kb-accept-entry = "Tab,KP_Enter";
+                kb-remove-to-eol = "Control+K";
+                kb-element-next = "Shift+Tab";
+                kb-element-prev = "ISO_Left_Tab";
+            };
         };
     };
 }
