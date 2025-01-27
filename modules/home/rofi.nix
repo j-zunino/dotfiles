531b5e807fa3c795cd43e4069856ccefcf86b4e7
j-zunino <zuninojuanbautista@gmail.com>
2025-01-26 21:43:44 -0300

    feat(rofi): add accept entry keybinds

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -9,7 +9,7 @@
             extraConfig = {
                 kb-row-up = "Up,Control+k";
                 kb-row-down = "Down,Control+j";
-                kb-accept-entry = "Tab,KP_Enter";
+                kb-accept-entry = "Tab,Control+m,Return,KP_Enter";
                 kb-remove-to-eol = "Control+K";
                 kb-element-next = "Shift+Tab";
                 kb-element-prev = "ISO_Left_Tab";
