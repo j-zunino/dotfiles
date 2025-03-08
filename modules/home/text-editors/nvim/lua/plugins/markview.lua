e2dba9268edaa64850bfb240c92594382fde3e75
j-zunino <zuninojuanbautista@gmail.com>
2025-03-08 05:13:17 -0300

    feat(nvim): support avante markdown

--- modules/home/text-editors/nvim/lua/plugins/markview.lua
+++ modules/home/text-editors/nvim/lua/plugins/markview.lua
@@ -2,8 +2,23 @@ return {
     'OXY2DEV/markview.nvim',
     lazy = true,
     event = { 'BufReadPost', 'BufNewFile' },
+    ft = { 'markdown', 'quarto', 'rmd', 'typst', 'Avante' },
     opts = {
         preview = {
+            filetypes = {
+                'md',
+                'markdown',
+                'norg',
+                'rmd',
+                'org',
+                'vimwiki',
+                'typst',
+                'latex',
+                'quarto',
+                'Avante',
+                'codecompanion',
+            },
+            ignore_buftypes = {},
             icon_provider = 'mini',
         },
     },
