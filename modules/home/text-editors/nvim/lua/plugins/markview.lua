565f029ffff2ce5be8a318eb8d41f8c71b868ed1
j-zunino <zuninojuanbautista@gmail.com>
2025-03-17 16:41:25 -0300

    refactor(nvim): simplify markview configuration

--- modules/home/text-editors/nvim/lua/plugins/markview.lua
+++ modules/home/text-editors/nvim/lua/plugins/markview.lua
@@ -1,22 +1,13 @@
 return {
     'OXY2DEV/markview.nvim',
     lazy = true,
-    event = { 'BufReadPost', 'BufNewFile' },
-    ft = { 'markdown', 'quarto', 'rmd', 'typst', 'Avante' },
+    ft = { 'markdown', 'Avante' },
     opts = {
         preview = {
             filetypes = {
                 'md',
                 'markdown',
-                'norg',
-                'rmd',
-                'org',
-                'vimwiki',
-                'typst',
-                'latex',
-                'quarto',
                 'Avante',
-                'codecompanion',
             },
             ignore_buftypes = {},
             icon_provider = 'mini',
