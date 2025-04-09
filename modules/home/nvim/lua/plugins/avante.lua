28c8411df03389c099986052e8d9db76b422bade
j-zunino <zuninojuanbautista@gmail.com>
2025-04-08 23:52:23 -0300

    revert(nvim): update avante model to gemini-2.5-pro-exp-03-25

--- modules/home/nvim/lua/plugins/avante.lua
+++ modules/home/nvim/lua/plugins/avante.lua
@@ -15,7 +15,7 @@ return {
     opts = {
         provider = 'gemini',
         gemini = {
-            model = 'gemini-2.5-pro-exp-03-25',
+            model = 'gemini-2.0-flash',
             temperature = 0,
             max_tokens = 4096,
         },
