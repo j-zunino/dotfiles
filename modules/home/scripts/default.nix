358ab57797e491c4c2997b5360f12b31965c6e2f
j-zunino <zuninojuanbautista@gmail.com>
2025-05-19 19:09:37 -0300

    feat(scripts): add tailwind color picker script

--- modules/home/scripts/default.nix
+++ modules/home/scripts/default.nix
@@ -9,12 +9,15 @@
     dm-clipboard = pkgs.writeScriptBin "clipboard" ''
         ${pkgs.stdenv.shell} ${config.home.homeDirectory}/dotfiles/modules/home/scripts/clipboard.sh
     '';
+    dm-tailwind = pkgs.writeScriptBin "tailwind" ''
+        ${pkgs.stdenv.shell} ${config.home.homeDirectory}/dotfiles/modules/home/scripts/tailwind.sh
     '';
 in {
     home = {
         packages = [
             dm-logout
             dm-clipboard
+            dm-tailwind
         ];
     };
 }
