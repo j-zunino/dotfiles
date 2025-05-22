a51245f29109c06aaf2bfde55243d0d69c62802c
j-zunino <zuninojuanbautista@gmail.com>
2025-05-22 14:40:40 -0300

    feat(nixos/wsl): update zellij theme

--- modules/home/themes.nix
+++ modules/home/themes.nix
@@ -1,4 +1,8 @@
-{lib, ...}: let
+{
+    lib,
+    pkgs,
+    ...
+}: let
     hex = c: c;
     rgb = c: let
         r = builtins.substring 1 2 c;
@@ -207,4 +211,35 @@ in
                 };
             };
         };
+
+        xdg.configFile."zellij/layouts/default.kdl".text = ''
+            layout {
+                pane split_direction="vertical" {
+                    pane
+                }
+
+                pane size=1 borderless=true {
+                    plugin location="file:${pkgs.zjstatus}/bin/zjstatus.wasm" {
+                        format_left "#[bg=${bg1-hex}]{mode}{tabs}"
+                        format_right "#[bg=${bg1-hex},fg=${grey1-hex}]{session} | {datetime}"
+                        format_space "#[bg=${bg1-hex}]"
+
+                        mode_normal "#[bg=${accent-hex},fg=${bg0-hex},bold] NORMAL #[bg=${bg1-hex},fg=${accent-hex}]"
+                        mode_locked  "#[bg=${red-hex},fg=${bg0-hex},bold] LOCKED #[bg=${bg1-hex},fg=${red-hex}]"
+                        mode_scroll  "#[bg=${aqua-hex},fg=${bg0-hex},bold] SCROLL #[bg=${bg1-hex},fg=${aqua-hex}]"
+                        mode_session "#[bg=${purple-hex},fg=${bg0-hex},bold] SESSION #[bg=${bg1-hex},fg=${purple-hex}]"
+
+                        tab_normal "#[bg=${bg1-hex},fg=${grey1-hex}] {index}: {name} {fullscreen_indicator}{sync_indicator}{floating_indicator}"
+                        tab_active "#[bg=${bg1-hex},fg=${accent-hex},bold] {index}: {name} {fullscreen_indicator}{sync_indicator}{floating_indicator}"
+                        tab_fullscreen_indicator "󰊓"
+                        tab_sync_indicator "󰓦"
+                        tab_floating_indicator "󰉈"
+
+                        datetime "#[bg=${bg1-hex},fg=${grey1-hex}]{format} "
+                        datetime_format "%A, %d %b %Y %H:%M"
+                        datetime_timezone "Argentina"
+                    }
+                }
+            }
+        '';
     }
