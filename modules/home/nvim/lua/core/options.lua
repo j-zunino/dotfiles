91f2aba89ee594c09e1a5f7b8c60eeac62fd28c6
j-zunino <zuninojuanbautista@gmail.com>
2025-06-05 17:38:38 -0300

    feat(nvim): add autocmds

--- modules/home/nvim/lua/core/options.lua
+++ modules/home/nvim/lua/core/options.lua
@@ -62,18 +62,3 @@ vim.opt.timeoutlen = 300
 vim.opt.backspace = 'indent,eol,start'
 
 vim.opt.pumheight = 15
-
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'snacks_dashboard',
-    callback = function()
-        vim.o.winborder = 'none'
-    end,
-})
-
-vim.api.nvim_create_autocmd('BufEnter', {
-    callback = function()
-        if vim.bo.filetype ~= 'snacks_dashboard' then
-            vim.o.winborder = 'single'
-        end
-    end,
-})
