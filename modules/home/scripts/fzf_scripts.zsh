6dd07dda823b06f2842892fbae4c9dfbb853a859
j-zunino <zuninojuanbautista@gmail.com>
2025-06-06 01:45:34 -0300

    feat(zsh): move fzf scripts, zellij auto name tab and prompt to separated files

---
+++ modules/home/scripts/fzf_scripts.zsh
@@ -0,0 +1,52 @@
+# Fzf: Open file
+fzf_file() {
+    local file
+    file=$(fd --type f | fzf \
+        --border-label=" Select a file to open " \
+        --bind "ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down") || return
+    [[ -n "$file" ]] && nvim "$file"
+    zle reset-prompt
+}
+
+# Fzf: Zoxide
+fzf_zoxide() {
+    local dir
+    dir=$(zoxide query -l | fzf \
+        --no-preview \
+        --border-label=' Select a folder to cd into ') || return
+    [[ -n "$dir" ]] && z "$dir"
+    precmd
+    zle reset-prompt
+}
+
+# Fzf: Live grep
+live_grep() {
+    local query result file linenumber
+
+    if [[ -z "$1" ]]; then
+        read -r "query?Enter search pattern: "
+    else
+        query="$*"
+    fi
+
+    result=$(rg --ignore-case --color=always --line-number --no-heading "$query" 2>/dev/null |
+        fzf --ansi \
+            --delimiter ":" \
+            --preview "bat --style=numbers --color=always {1} --highlight-line {2}" \
+            --preview-window "+{2}+3/3,~3" \
+            --bind "ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" )
+
+    file="''${result%%:*}"
+    linenumber="$(echo "''${result}" | cut -d: -f2)"
+    if [[ -n "$file" ]]; then
+        $EDITOR +"$linenumber" "$file"
+    fi
+}
+
+zle -N fzf_file
+zle -N fzf_zoxide
+zle -N live_grep
+
+bindkey "^F" fzf_file
+bindkey "^Z" fzf_zoxide
+bindkey "^G" live_grep
--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -7,137 +7,15 @@
         oh-my-zsh.enable = true;
 
         initContent = ''
-            autoload -Uz vcs_info
-
             KEYTIMEOUT=1
             bindkey -v
-
-            # Prompt
-            zstyle ':vcs_info:*' enable git
-            zstyle ':vcs_info:*' formats '%F{yellow} %b%f%m'
-            zstyle ':vcs_info:*' actionformats '%F{yellow} %b%f %F{blue}%a%f'
-            zstyle ':vcs_info:git*+set-message:*' hooks git-check
-
-            +vi-git-check() {
-              if [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == 'true' ]]; then
-                local GIT_STATUS UNPUSHED UNPULLED STAGED UNSTAGED UNTRACKED
-
-                UNPUSHED=$(git log --oneline @{u}.. 2>/dev/null | wc -l)
-                [[ $UNPUSHED -gt 0 ]] && hook_com[misc]+=" %F{blue}↑$UNPUSHED%f"
-
-                UNPULLED=$(git log --oneline ..@{u} 2>/dev/null | wc -l)
-                [[ $UNPULLED -gt 0 ]] && hook_com[misc]+=" %F{magenta}↓$UNPULLED%f"
-
-                GIT_STATUS=$(git status --porcelain)
-
-                STAGED=$(echo "$GIT_STATUS" | grep -v '??' | grep -E '^[^ ]' | wc -l)
-                [[ $STAGED   -gt 0 ]] && hook_com[misc]+=" %F{green}+$STAGED%f"
-
-                UNSTAGED=$(echo "$GIT_STATUS" | grep -E '^ ' | wc -l)
-                [[ $UNSTAGED -gt 0 ]] && hook_com[misc]+=" %F{yellow}*$UNSTAGED%f"
-
-                UNTRACKED=$(echo "$GIT_STATUS" | grep '^??' | wc -l)
-                [[ $UNTRACKED -gt 0 ]] && hook_com[misc]+=" %F{red}??$UNTRACKED%f"
-              fi
-            }
-
-            setopt PROMPT_SUBST
-            export PS1='%B%F{red}󰉋 %~%f $(print -P "$vcs_info_msg_0_")
-            %B%F{green} %f%b '
-
-
-            # Vim cursor
-            zle-keymap-select () {
-                if [[ $KEYMAP == vicmd ]]; then
-                    echo -ne "\e[2 q"
-                else
-                    echo -ne "\e[5 q"
-                fi
-            }
-            precmd_functions+=(zle-keymap-select)
-            zle -N zle-keymap-select
-
-
-            precmd() {
-              psvar=()
-              vcs_info
-              [[ -n $vcs_info_msg_0_ ]] && print -v 'psvar[1]' -Pr -- "$vcs_info_msg_0_"
-              zle-keymap-select  # mantener el cursor estilo vim
-            }
-
 
             # Zoxide
             eval "$(zoxide init zsh)"
 
-
-            # Zellij rename tab
-            zellij_tab_name_update() {
-                if [[ -n $ZELLIJ ]]; then
-                    local current_dir=$PWD
-                    if [[ $current_dir == $HOME ]]; then
-                        current_dir="~"
-                    else
-                        current_dir="''${current_dir##*/}"
-                    fi
-                    command nohup zellij action rename-tab $current_dir >/dev/null 2>&1
-                fi
-            }
-
-            zellij_tab_name_update
-            chpwd_functions+=(zellij_tab_name_update)
-
-
-            # Fzf
-            fzf_file() {
-              local file
-              file=$(fd --type f | fzf \
-                --border-label=" Select a file to open " \
-                --bind "ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down") || return
-              [[ -n "$file" ]] && nvim "$file"
-              zle reset-prompt
-            }
-
-            fzf_zoxide() {
-              local dir
-              dir=$(zoxide query -l | fzf \
-                --no-preview \
-                --border-label=' Select a folder to cd into ') || return
-              [[ -n "$dir" ]] && z "$dir"
-
-              precmd
-              zle reset-prompt
-            }
-
-            live_grep() {
-              local query result file linenumber
-
-              if [[ -z "$1" ]]; then
-                read -r "query?Enter search pattern: "
-              else
-                query="$*"
-              fi
-
-              result=$(rg --ignore-case --color=always --line-number --no-heading "$query" 2>/dev/null |
-                fzf --ansi \
-                    --delimiter ":" \
-                    --preview "bat --style=numbers --color=always {1} --highlight-line {2}" \
-                    --preview-window "+{2}+3/3,~3" \
-                    --bind "ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" )
-
-              file="''${result%%:*}"
-              linenumber="$(echo "''${result}" | cut -d: -f2)"
-              if [[ -n "$file" ]]; then
-                $EDITOR +"$linenumber" "$file"
-              fi
-            }
-
-            zle -N fzf_file
-            zle -N fzf_zoxide
-            zle -N live_grep
-
-            bindkey "^F" fzf_file
-            bindkey "^Z" fzf_zoxide
-            bindkey "^G" live_grep
+            source $HOME/dotfiles/modules/home/scripts/promp.zsh
+            source $HOME/dotfiles/modules/home/scripts/fzf_scripts.zsh
+            source $HOME/dotfiles/modules/home/scripts/zellij_tabs.zsh
 
             bindkey '^J' history-search-backward
             bindkey '^K' history-search-forward
