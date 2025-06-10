06cf65bd79b2f80b01dc31c9df2afb078dec1ea4
j-zunino <zuninojuanbautista@gmail.com>
2025-06-10 01:59:07 -0300

    feat(nvim): change minty color picker keybinds

--- modules/home/nvim/lua/plugins/minty.lua
+++ modules/home/nvim/lua/plugins/minty.lua
@@ -4,15 +4,15 @@ return {
     {
         'nvzone/minty',
         cmd = { 'Shades', 'Huefy' },
-        keys = { '<leader>cp', '<leader>cs' },
+        keys = { '<leader>Cp', '<leader>Cs' },
         config = function()
             local opts = { noremap = true, silent = true }
 
-            opts.desc = 'Color picker'
-            vim.keymap.set('n', '<leader>cp', '<cmd>Huefy<CR>', opts)
+            opts.desc = 'Picker'
+            vim.keymap.set('n', '<leader>Cp', '<cmd>Huefy<CR>', opts)
 
-            opts.desc = 'Color shades'
-            vim.keymap.set('n', '<leader>cs', '<cmd>Shades<CR>', opts)
+            opts.desc = 'Shades'
+            vim.keymap.set('n', '<leader>Cs', '<cmd>Shades<CR>', opts)
 
             require('minty').setup({
                 huefy = {
