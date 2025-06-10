3df3ba2e01bd555b81cc03d978cb3dc0ec87bd08
j-zunino <zuninojuanbautista@gmail.com>
2025-06-10 02:28:10 -0300

    refactor(nvim): update which-key config

--- modules/home/nvim/lua/plugins/which-key.lua
+++ modules/home/nvim/lua/plugins/which-key.lua
@@ -1,88 +1,39 @@
 return {
     'folke/which-key.nvim',
-    keys = '<leader>',
     opts_extend = { 'spec' },
-    opts = {
-        preset = 'helix',
-        defaults = {},
-        spec = {
-            {
-                mode = { 'n', 'v' },
-                {
-                    '<leader>a',
-                    group = 'Avante',
-                    icon = { icon = '', color = 'yellow' },
-                },
-                {
-                    '<leader>c',
-                    group = 'Code',
-                    icon = { icon = ' ', color = 'red' },
-                },
-                {
-                    '<leader>d',
-                    group = 'Document',
-                    icon = { icon = '󰈙', color = 'cyan' },
-                },
-                {
-                    '<leader>r',
-                    group = 'Rename',
-                    icon = { icon = '' },
-                },
-                {
-                    '<leader>R',
-                    group = 'Rest',
-                    icon = { icon = '' },
-                },
-                {
-                    '<leader>s',
-                    group = 'Snacks picker',
-                    icon = { icon = '󱥰', color = 'purple' },
-                },
-                {
-                    '<leader>w',
-                    group = 'Workspace',
-                    icon = { icon = '', color = 'blue' },
-                },
-                {
-                    '<leader>ts',
-                    group = 'Typescript',
-                    icon = { icon = '', color = 'cyan' },
-                },
-                {
-                    '<leader>tw',
-                    group = 'Tailwind',
-                    icon = { icon = '󱏿', color = 'cyan' },
-                },
-                {
-                    '<leader>g',
-                    group = 'Git',
-                    icon = { icon = '', color = 'orange' },
-                },
-                {
-                    '<leader>gh',
-                    group = 'Hunks',
-                    icon = { icon = '', color = 'orange' },
-                },
-                {
-                    '<leader>l',
-                    group = 'Lazygit',
-                    icon = { icon = '', color = 'orange' },
-                },
-                {
-                    '<leader>lg',
-                    group = 'Lazygit',
-                    icon = { icon = '', color = 'orange' },
-                },
-                {
-                    '<leader>b',
-                    group = 'Buffers',
-                    icon = { icon = '', color = 'red' },
-                },
-            },
-        },
+    config = function()
+        local wk = require('which-key')
 
-        win = {
-            border = 'single',
-        },
-    },
+        ---@diagnostic disable-next-line: missing-fields
+        wk.setup({
+            preset = 'helix',
+            delay = 200,
+            notify = true,
+            win = { border = 'single' },
+            defaults = {},
+        })
+
+        -- stylua: ignore start
+        wk.add({
+            {
+                mode = { 'n' },
+                { '<leader>a', group = 'Avante', icon = { icon = '', color = 'yellow' }, },
+                { '<leader>b', group = 'Buffers', icon = { icon = '', color = 'red' }, },
+                { '<leader>C', group = 'Color', icon = { icon = ' ', color = 'red' }, },
+                { '<leader>c', group = 'Code', icon = { icon = ' ', color = 'red' }, },
+                { '<leader>ct', group = 'Typescript', icon = { icon = '', color = 'cyan' }, },
+                { '<leader>d', group = 'Document', icon = { icon = '󰈙', color = 'cyan' }, },
+                { '<leader>g', group = 'Git', icon = { icon = '', color = 'orange' }, },
+                { '<leader>gh', group = 'Hunks', icon = { icon = '', color = 'orange' }, },
+                { '<leader>l', group = 'Lazygit', icon = { icon = '', color = 'orange' }, },
+                { '<leader>lg', group = 'Lazygit', icon = { icon = '', color = 'orange' }, },
+                { '<leader>r', group = 'Rename', icon = { icon = '' }, },
+                { '<leader>R', group = 'Rest', icon = { icon = '' }, },
+                { '<leader>s', group = 'Snacks picker', icon = { icon = '󱥰', color = 'purple' }, },
+                { '<leader>w', group = 'Tailwind', icon = { icon = '󱏿', color = 'cyan' }, },
+                { '<leader>T', group = 'Toggle', },
+            },
+        })
+        -- stylua: ignore end
+    end,
 }
