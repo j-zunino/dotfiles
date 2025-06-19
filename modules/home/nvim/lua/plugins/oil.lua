1f6540c318fcf28505314471eb574100c96d1f8f
j-zunino <zuninojuanbautista@gmail.com>
2025-06-19 00:41:22 -0300

    feat(nvim): replace snacks explorer whit oil.nvim

---
+++ modules/home/nvim/lua/plugins/oil.lua
@@ -0,0 +1,37 @@
+return {
+    'stevearc/oil.nvim',
+    dependencies = { 'echasnovski/mini.icons' },
+
+    config = function()
+        vim.keymap.set('n', '-', ':Oil<CR>', { desc = 'Open parent directory' })
+        vim.keymap.set(
+            'n',
+            '<C-e>',
+            ':Oil<CR>',
+            { desc = 'Open parent directory' }
+        )
+
+        require('oil').setup({
+            default_file_explorer = true,
+
+            win_options = {
+                signcolumn = 'yes',
+            },
+            view_options = {
+                show_hidden = true,
+            },
+            confirmation = {
+                border = 'single',
+            },
+            progress = {
+                border = 'single',
+            },
+            ssh = {
+                border = 'single',
+            },
+            keymaps_help = {
+                border = 'single',
+            },
+        })
+    end,
+}
