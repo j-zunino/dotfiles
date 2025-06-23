0df10a5d66a27e555fc88c838685ae822736f91f
j-zunino <zuninojuanbautista@gmail.com>
2025-06-23 00:55:31 -0300

    feat(zsh/zellij): add zellij sessionizer script

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -10,12 +10,12 @@
             KEYTIMEOUT=1
             bindkey -v
 
-            # Zoxide
             eval "$(zoxide init zsh)"
 
             source $HOME/dotfiles/modules/home/scripts/promp.zsh
             source $HOME/dotfiles/modules/home/scripts/fzf_scripts.zsh
             source $HOME/dotfiles/modules/home/scripts/zellij_tabs.zsh
+            source $HOME/dotfiles/modules/home/scripts/zellij_sessionizer.zsh
 
             bindkey '^J' history-search-backward
             bindkey '^K' history-search-forward
