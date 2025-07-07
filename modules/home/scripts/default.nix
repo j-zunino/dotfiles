fd3b41ca3b892792ae5f4740ed8815ee6675b19e
j-zunino <zuninojuanbautista@gmail.com>
2025-07-07 16:44:11 -0300

    refactor(scripts): move scripts into subdirectories

--- modules/home/scripts/default.nix
+++ modules/home/scripts/default.nix
@@ -4,19 +4,19 @@
     ...
 }: let
     rofi_logout = pkgs.writeScriptBin "rofi_logout" ''
-        ${pkgs.stdenv.shell} ${config.home.homeDirectory}/dotfiles/modules/home/scripts/rofi_logout.sh
+        ${pkgs.stdenv.shell} $HOME/dotfiles/modules/home/scripts/rofi/rofi_logout.sh
     '';
     rofi_clipboard = pkgs.writeScriptBin "rofi_clipboard" ''
-        ${pkgs.stdenv.shell} ${config.home.homeDirectory}/dotfiles/modules/home/scripts/rofi_clipboard.sh
+        ${pkgs.stdenv.shell} $HOME/dotfiles/modules/home/scripts/rofi/rofi_clipboard.sh
     '';
     rofi_tailwind = pkgs.writeScriptBin "tailwind" ''
-        ${pkgs.stdenv.shell} ${config.home.homeDirectory}/dotfiles/modules/home/scripts/rofi_tailwind.sh
+        ${pkgs.stdenv.shell} $HOME/dotfiles/modules/home/scripts/rofi/rofi_tailwind.sh
     '';
     battery_status = pkgs.writeScriptBin "battery_status" ''
-        ${pkgs.stdenv.shell} ${config.home.homeDirectory}/dotfiles/modules/home/scripts/battery_status.sh
+        ${pkgs.stdenv.shell} $HOME/dotfiles/modules/home/scripts/battery_status.sh
     '';
     network_status = pkgs.writeScriptBin "network_status" ''
-        ${pkgs.stdenv.shell} ${config.home.homeDirectory}/dotfiles/modules/home/scripts/network_status.sh
+        ${pkgs.stdenv.shell} $HOME/dotfiles/modules/home/scripts/network_status.sh
     '';
 in {
     home = {
