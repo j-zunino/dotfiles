ecb9c92f83c0ecf747bc255af590910f402b7e7f
j-zunino <zuninojuanbautista@gmail.com>
2025-07-24 01:42:54 -0300

    refactor(nvim): move mini.icons config to dedicated file
    fix(nvim): import mini.icons

--- modules/home/nvim/lua/plugins/mini.lua
+++ modules/home/nvim/lua/plugins/mini.lua
@@ -2,8 +2,6 @@ return {
     'echasnovski/mini.nvim',
     version = '*',
     config = function()
-        local dir = { glyph = '󰉋', hl = 'Function' }
-
         require('mini.pairs').setup()
         require('mini.surround').setup()
         require('mini.ai').setup({ n_lines = 80 })
@@ -39,60 +37,6 @@ return {
                 },
             },
         })
-        require('mini.icons').setup({
-            require('mini.icons').mock_nvim_web_devicons(),
-
-            default = {
-                directory = dir,
-            },
-            directory = {
-                ['.git'] = dir,
-                ['.github'] = dir,
-                Desktop = dir,
-                Library = dir,
-                Network = dir,
-                Public = dir,
-                home = dir,
-                lang = dir,
-                lib = dir,
-                lsp = dir,
-                media = dir,
-                queries = dir,
-                root = dir,
-                src = dir,
-                snippets = dir,
-                syntax = dir,
-            },
-            extension = {
-                html = { glyph = '' },
-                json = { glyph = '' },
-                json5 = { glyph = '' },
-                jsonc = { glyph = '' },
-                jsonl = { glyph = '' },
-                lock = { glyph = '' },
-                md = { glyph = '' },
-
-                js = { glyph = '' },
-                ts = { glyph = '' },
-                jsx = { glyph = '' },
-                tsx = { glyph = '' },
-
-                nix = { glyph = '' },
-
-                jpeg = { glyph = '', hl = 'MiniIconsOrange' },
-                jpg = { glyph = '', hl = 'MiniIconsOrange' },
-                png = { glyph = '', hl = 'MiniIconsPurple' },
-                tif = { glyph = '', hl = 'MiniIconsYellow' },
-                tiff = { glyph = '', hl = 'MiniIconsYellow' },
-                webp = { glyph = '', hl = 'MiniIconsBlue' },
-            },
-            file = {
-                README = { glyph = '', hl = 'MiniIconsGray' },
-                ['README.md'] = { glyph = '', hl = 'MiniIconsGray' },
-                ['README.txt'] = { glyph = '', hl = 'MiniIconsGray' },
-                TODO = { glyph = '' },
-                ['TODO.md'] = { glyph = '' },
-            },
-        })
+        require('plugins.mini.icons')
     end,
 }
