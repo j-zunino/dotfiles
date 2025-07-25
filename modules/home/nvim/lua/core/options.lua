032aad065bf35e4192965799b957aee1da7201da
j-zunino <zuninojuanbautista@gmail.com>
2025-07-25 16:02:30 -0300

    refactor(nvim): reorganize options and keymaps

--- modules/home/nvim/lua/core/options.lua
+++ modules/home/nvim/lua/core/options.lua
@@ -1,58 +1,63 @@
+---------------------------------------------------------------
+-- GENERAL
+---------------------------------------------------------------
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.numberwidth = 2
-vim.opt.cmdheight = 1
-vim.opt.showmode = false
 vim.opt.cursorline = true
+vim.opt.signcolumn = 'yes'
+vim.opt.colorcolumn = '80'
 vim.opt.termguicolors = true
+vim.opt.showmode = false
 
+vim.opt.scrolloff = 8
+vim.opt.pumheight = 15
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.wrap = false
+vim.opt.linebreak = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.opt.undofile = true
+vim.opt.swapfile = false
+vim.opt.backspace = 'indent,eol,start'
+
+vim.opt.updatetime = 100
+vim.opt.timeoutlen = 300
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+---------------------------------------------------------------
+-- SEARCH
+---------------------------------------------------------------
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.inccommand = 'split'
 
+---------------------------------------------------------------
+-- INDENT
+---------------------------------------------------------------
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
-
-vim.opt.wrap = false
-vim.opt.linebreak = true
-vim.opt.breakindent = true
 vim.opt.smartindent = true
+vim.opt.breakindent = true
 
-vim.o.foldmethod = 'expr'
-vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.o.foldtext = 'v:lua.custom_fold_text()'
-vim.o.foldenable = true
-vim.o.foldlevel = 99
+---------------------------------------------------------------
+-- FOLD
+---------------------------------------------------------------
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldtext = 'v:lua.custom_fold_text()'
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99
 
-vim.opt.undofile = true
-vim.opt.swapfile = false
-vim.cmd('let g:netrw_liststlye = 3')
-
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
-vim.opt.signcolumn = 'yes'
-
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.mouse = 'a'
-vim.opt.scrolloff = 8
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
-vim.opt.updatetime = 100
-vim.opt.timeoutlen = 300
-vim.opt.inccommand = 'split'
-
-vim.opt.backspace = 'indent,eol,start'
-
-vim.opt.pumheight = 15
-
-vim.opt.colorcolumn = '80'
-
--- Other
 function _G.custom_fold_text()
     local line = vim.fn.getline(vim.v.foldstart)
     local lines = vim.v.foldend - vim.v.foldstart + 1
