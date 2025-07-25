7b59ce332c6ba31c57504ce1dda4cd838d7034bb
j-zunino <zuninojuanbautista@gmail.com>
2025-07-25 16:02:30 -0300

    refactor(nvim): reorganize options and keymaps

--- modules/home/nvim/lua/core/keymaps.lua
+++ modules/home/nvim/lua/core/keymaps.lua
@@ -8,40 +8,54 @@ local map = function(mode, keys, func, desc, opts)
     vim.keymap.set(mode, keys, func, options)
 end
 
+-------------------------------------------------------------------------------
+-- GENERAL
+-------------------------------------------------------------------------------
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
-map({ 'n', 'v' }, '<Space>', '<Nop>')
-
-map('n', '<leader>L', '<cmd>Lazy<CR>', 'Open Lazy')
-
-map('n', '<leader>j', 'mzJ`z', 'Join lines')
-
-map('n', '<leader>Tw', '<cmd>set wrap!<CR>', 'Word wrap')
-
-map('n', 'U', '<C-r>', 'Undo')
-
-map('n', '<Esc>', ':nohls<CR>', 'Clear search highlight')
 
 map('n', '<C-s>', ':w<CR>', 'Save file')
 map('n', '<C-q>', ':q<CR>', 'Quit file')
 
-map('n', '<Tab>', ':bnext<CR>', 'Next buffer')
-map('n', '<S-Tab>', ':bprevious<CR>', 'Previous buffer')
+map('n', '<Esc>', ':nohls<CR>', 'Clear search highlight')
 
+map('n', '<leader>Tw', '<cmd>set wrap!<CR>', 'Toggle word wrap')
+
+-------------------------------------------------------------------------------
+-- EDITING
+-------------------------------------------------------------------------------
 map('n', '<leader>}', 'gcc', 'Comment line', { remap = true })
 map('v', '<leader>}', 'gc', 'Comment lines', { remap = true })
 
-map('n', 'x', '"_x', 'Delete without saving into register')
-map('n', 'X', '"_X', 'Delete without saving into register')
+map({ 'n', 'v' }, 'x', '"_x', 'Delete without saving into register')
+map({ 'n', 'v' }, 'X', '"_X', 'Delete without saving into register')
 map('v', 'p', '"_dP', 'Paste without saving into register')
 map('n', 'P', ':put!<CR>`[v`]=', 'Paste without saving into register')
 
-map({ 'n', 'v' }, 'J', '5j', 'Move down 5 lines')
-map({ 'n', 'v' }, 'K', '5k', 'Move up 5 lines')
+map('n', 'Y', 'yy', 'Yank line')
+map('n', 'yA', ':%y<CR>', 'Yank all')
+map('n', 'yJ', 'Yp', 'Yank line down')
+map('n', 'yK', 'YP', 'Yank line up')
 
 map('v', '<C-j>', ":move '>+1<CR>gv=gv", 'Move line down')
 map('v', '<C-k>', ":move '<-2<CR>gv=gv", 'Move line up')
+
+map('n', '<leader>j', 'mzJ`z', 'Join lines')
+
+map('n', 'U', '<C-r>', 'Redo')
+
+map(
+    'n',
+    '<leader>X',
+    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+    'Replace word under cursor'
+)
+
+-------------------------------------------------------------------------------
+-- NAVIGATION
+-------------------------------------------------------------------------------
+map({ 'n', 'v' }, 'J', '5j', 'Move down 5 lines')
+map({ 'n', 'v' }, 'K', '5k', 'Move up 5 lines')
 
 map({ 'n', 'v' }, '<C-d>', '<C-d>zz', 'Scroll down')
 map({ 'n', 'v' }, '<C-u>', '<C-u>zz', 'Scroll up')
@@ -49,13 +63,17 @@ map({ 'n', 'v' }, '<C-u>', '<C-u>zz', 'Scroll up')
 map('n', 'n', 'nzz', 'Find next')
 map('n', 'N', 'Nzz', 'Find previous')
 
-map('n', 'Y', 'yy', 'Copy line')
-map('n', 'yA', ':%y<cr>', 'Copy all')
-map('n', 'yJ', 'Yp', 'Copy line down')
-map('n', 'yK', 'YP', 'Copy line up')
+-------------------------------------------------------------------------------
+-- BUFFERS
+-------------------------------------------------------------------------------
+map('n', '<Tab>', ':bnext<CR>', 'Next buffer')
+map('n', '<S-Tab>', ':bprevious<CR>', 'Previous buffer')
 
-map('n', '<C-j>', '<C-w>w', 'Move focus to the next window')
-map('n', '<C-k>', '<C-w>W', 'Move focus to the previous window')
+-------------------------------------------------------------------------------
+-- WINDOWS / SPLITS
+-------------------------------------------------------------------------------
+map('n', '<C-j>', '<C-w>w', 'Focus next window')
+map('n', '<C-k>', '<C-w>W', 'Focus previous window')
 
 map('n', '<C-w>s', ':split<CR>', 'Split horizontal')
 map('n', '<C-w>v', ':vsplit<CR>', 'Split vertical')
@@ -65,6 +83,16 @@ map('n', '<C-h>', ':vertical resize -5<CR>', 'Decrease split width')
 map('n', '+', '<C-w>+', 'Increase split height')
 map('n', '-', '<C-w>-', 'Decrease split height')
 
-map('n', '<C-f>', ':silent !tmux neww tmux_sessionizer<CR>')
+-------------------------------------------------------------------------------
+-- MISC
+-------------------------------------------------------------------------------
+map('n', '<leader>L', ':Lazy<CR>', 'Open Lazy')
 
-map('n', '<leader>X', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map('n', 'Q', '<nop>', 'Disable Ex mode')
+
+map(
+    'n',
+    '<C-f>',
+    ':silent !tmux neww tmux_sessionizer<CR>',
+    'Open Tmux sessionizer'
+)
