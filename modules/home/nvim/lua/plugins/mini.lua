b77e54b50ff4995c15a3b3e1de0619fb31f3c57e
j-zunino <zuninojuanbautista@gmail.com>
2025-07-27 14:39:47 -0300

    feat(nvim): enhance text object selection with mini.ai

--- modules/home/nvim/lua/plugins/mini.lua
+++ modules/home/nvim/lua/plugins/mini.lua
@@ -4,8 +4,8 @@ return {
     config = function()
         require('mini.pairs').setup()
         require('mini.surround').setup()
-        require('mini.ai').setup({ n_lines = 80 })
         require('plugins.mini.hipatterns')
+        require('plugins.mini.ai')
         require('plugins.mini.icons')
         require('plugins.mini.statusline')
     end,
