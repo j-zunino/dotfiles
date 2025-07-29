ded7eba823b21ee0374be0185046ed1392baaebb
j-zunino <zuninojuanbautista@gmail.com>
2025-07-28 21:49:13 -0300

    refactor(nvim): clean up blink config

--- modules/home/nvim/lua/plugins/blink.lua
+++ modules/home/nvim/lua/plugins/blink.lua
@@ -1,20 +1,10 @@
 return {
     'saghen/blink.cmp',
-    dependencies = {
-        'rafamadriz/friendly-snippets',
-        'saghen/blink.compat',
-        { 'L3MON4D3/LuaSnip', version = 'v2.*' },
-        {
-            'saghen/blink.compat',
-            version = '*',
-            lazy = true,
-            opts = {},
-        },
-    },
     version = '1.*',
     event = 'InsertEnter',
-    opts_extend = {
-        'sources.default',
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+        { 'L3MON4D3/LuaSnip', version = 'v2.*' },
     },
     opts = {
         keymap = {
@@ -23,24 +13,11 @@ return {
             ['<C-k>'] = { 'select_prev', 'fallback' },
         },
 
-        appearance = {
-            nerd_font_variant = 'mono',
-        },
-
         snippets = { preset = 'luasnip' },
+        signature = { enabled = true },
 
         sources = {
-            default = {
-                'lazydev',
-                'lsp',
-                'path',
-                'snippets',
-                'buffer',
-
-                'avante_commands',
-                'avante_mentions',
-                'avante_files',
-            },
+            default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
 
             providers = {
                 lazydev = {
@@ -48,45 +25,13 @@ return {
                     module = 'lazydev.integrations.blink',
                     score_offset = 100,
                 },
-
-                avante_files = {
-                    name = 'avante_files',
-                    module = 'blink.compat.source',
-                    score_offset = 100,
-                    enabled = function()
-                        return vim.bo.filetype == 'AvanteInput'
-                    end,
-                },
-                avante_mentions = {
-                    name = 'avante_mentions',
-                    module = 'blink.compat.source',
-                    score_offset = 1000,
-                    enabled = function()
-                        return vim.bo.filetype == 'AvanteInput'
-                    end,
-                },
-                avante_commands = {
-                    name = 'avante_commands',
-                    module = 'blink.compat.source',
-                    score_offset = 90,
-                    enabled = function()
-                        return vim.bo.filetype == 'AvanteInput'
-                    end,
-                },
-            },
-        },
-
-        signature = {
-            enabled = true,
-            window = {
-                winblend = 0,
             },
         },
 
         completion = {
-            ghost_text = {
-                enabled = false,
-            },
+            ghost_text = { enabled = false },
+            documentation = { auto_show = true },
+            accept = { auto_brackets = { enabled = false } },
 
             list = {
                 selection = {
@@ -95,16 +40,7 @@ return {
                 },
             },
 
-            documentation = {
-                auto_show = true,
-                window = {
-                    winblend = 0,
-                },
-            },
-
             menu = {
-                min_width = 20,
-                scrollbar = true,
                 draw = {
                     align_to = 'cursor',
                     columns = {
@@ -115,4 +51,5 @@ return {
             },
         },
     },
+    opts_extend = { 'sources.default' },
 }
