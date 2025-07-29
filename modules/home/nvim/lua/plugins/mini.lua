4f0173b0c95ca601d24ba1d79365798baaf2a79e
j-zunino <zuninojuanbautista@gmail.com>
2025-07-28 21:43:52 -0300

    refactor(nvim): reorder mini.lua

--- modules/home/nvim/lua/plugins/mini.lua
+++ modules/home/nvim/lua/plugins/mini.lua
@@ -4,9 +4,9 @@ return {
     config = function()
         require('mini.pairs').setup()
         require('mini.surround').setup()
-        require('plugins.mini.hipatterns')
         require('plugins.mini.ai')
         require('plugins.mini.icons')
+        require('plugins.mini.hipatterns')
         require('plugins.mini.statusline')
         require('plugins.mini.indentscope')
     end,
