2c573f5f0fc578c69ffaa02001977a5af57d7a51
j-zunino <zuninojuanbautista@gmail.com>
2025-07-28 21:46:42 -0300

    feat(nvim): load template-string only for relevant fts

--- modules/home/nvim/lua/plugins/template-string.lua
+++ modules/home/nvim/lua/plugins/template-string.lua
@@ -1,20 +1,17 @@
 return {
     'axelvc/template-string.nvim',
-    lazy = true,
-    -- event = 'VeryLazy',
-    event = { 'BufReadPost', 'BufNewFile' },
+    ft = {
+        'html',
+        'vue',
+        'svelte',
+        'python',
+        'javascript',
+        'typescript',
+        'javascriptreact',
+        'typescriptreact',
+    },
     config = function()
         require('template-string').setup({
-            filetypes = {
-                'html',
-                'typescript',
-                'javascript',
-                'typescriptreact',
-                'javascriptreact',
-                'vue',
-                'svelte',
-                'python',
-            },
             jsx_brackets = true,
             remove_template_string = true,
             restore_quotes = {
