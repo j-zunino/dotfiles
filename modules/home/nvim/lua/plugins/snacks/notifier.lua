158b33e0d63a277c014a99f5327f9ec380b427f7
j-zunino <zuninojuanbautista@gmail.com>
2025-07-28 21:28:09 -0300

    feat(nvim): simplify snacks configs and dashboard layout, remove lazygit

--- modules/home/nvim/lua/plugins/snacks/notifier.lua
+++ modules/home/nvim/lua/plugins/snacks/notifier.lua
@@ -4,20 +4,20 @@ opts.desc = 'Show notifications history'
 vim.keymap.set(
     'n',
     '<leader>n',
-    '<cmd>lua Snacks.notifier.show_history()<CR>',
+    ':lua Snacks.notifier.show_history()<CR>',
     opts
 )
 
----@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
 local progress = vim.defaulttable()
 vim.api.nvim_create_autocmd('LspProgress', {
-    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+        local value = ev.data.params.value
+
         if not client or type(value) ~= 'table' then
             return
         end
+
         local p = progress[client.id]
 
         for i = 1, #p + 1 do
@@ -36,7 +36,7 @@ vim.api.nvim_create_autocmd('LspProgress', {
             end
         end
 
-        local msg = {} ---@type string[]
+        local msg = {}
         progress[client.id] = vim.tbl_filter(function(v)
             return table.insert(msg, v.msg) or not v.done
         end, p)
@@ -53,6 +53,7 @@ vim.api.nvim_create_autocmd('LspProgress', {
             '⠇',
             '⠏',
         }
+
         vim.notify(table.concat(msg, '\n'), 'info', {
             id = 'lsp_progress',
             title = client.name,
--- modules/home/nvim/lua/plugins/snacks/picker.lua
+++ modules/home/nvim/lua/plugins/snacks/picker.lua
@@ -1,32 +1,44 @@
-local opts = { noremap = true, silent = true }
+local map = function(mode, keys, func, desc, opts)
+    local options = vim.tbl_extend(
+        'force',
+        { noremap = true, silent = true, desc = desc },
+        opts or {}
+    )
+
+    vim.keymap.set(mode, keys, func, options)
+end
+
+map('n', '<leader>sf', ':lua Snacks.picker.files()<CR>', 'Files')
+map('n', '<leader>sg', ':lua Snacks.picker.grep()<CR>', 'Grep')
+map('n', '<leader>sw', ':lua Snacks.picker.grep_word()<CR>', 'Grep Word')
+map('n', '<leader>ss', ':lua Snacks.picker.spelling()<CR>', 'Spell suggestions')
+map('n', '<leader>sh', ':lua Snacks.picker.help()<CR>', 'Help')
+map('n', '<leader>sd', ':lua Snacks.picker.diagnostics()<CR>', 'Diagnostics')
+map('n', '<leader>si', ':lua Snacks.picker.icons()<CR>', 'Icons')
+map('n', '<leader>sc', ':lua Snacks.picker.colorschemes()<CR>', 'Colorschemes')
+map('n', '<leader><leader>', ':lua Snacks.picker.buffers()<CR>', 'Buffers')
 
 local picker_layout = {
     reverse = true,
     layout = {
-        box = 'horizontal',
-        width = 0.8,
-        height = 0.9,
-        border = 'none',
+        box = 'vertical',
+        width = 0.5,
+        height = 0.7,
+        border = 'single',
+        title = '{title} {live} {flags}',
+        {
+            win = 'preview',
+            border = 'bottom',
+            height = 0.5,
+        },
         {
             box = 'vertical',
-            border = 'single',
-            title = ' Results ',
-            title_pos = 'center',
             { win = 'list', border = 'none' },
             {
                 height = 1,
                 win = 'input',
                 border = 'top',
-                title = '{title} {live} {flags}',
-                title_pos = 'center',
             },
-        },
-        {
-            win = 'preview',
-            title = '{preview}',
-            width = 0.5,
-            border = 'single',
-            title_pos = 'center',
         },
     },
 }
@@ -55,90 +67,38 @@ local spelling_layout = {
     },
 }
 
-local explorer_layout = {
-    reverse = false,
-    layout = {
-        box = 'horizontal',
-        width = 0.5,
-        height = 0.9,
-        {
-            box = 'vertical',
-            border = 'single',
-            title = '{title} {live} {flags}',
-            title_pos = 'center',
-            {
-                height = 1,
-                win = 'input',
-                border = 'none',
-                title = ' Filter ',
-                title_pos = 'center',
-            },
-            { win = 'list', border = 'top' },
-            {
-                win = 'preview',
-                border = 'top',
-                title = '{preview}',
-                title_pos = 'center',
-            },
-        },
-    },
-}
-
--- opts.desc = 'File explorer'
--- vim.keymap.set('n', '<C-e>', ':lua Snacks.explorer.open()<CR>', opts)
-
-opts.desc = 'Files'
-vim.keymap.set('n', '<leader>sf', ':lua Snacks.picker.files()<CR>', opts)
-
--- Grep
-opts.desc = 'Grep'
-vim.keymap.set('n', '<leader>sg', ':lua Snacks.picker.grep()<CR>', opts)
-
-opts.desc = 'Word'
-vim.keymap.set('n', '<leader>sw', ':lua Snacks.picker.grep_word()<CR>', opts)
-
-opts.desc = 'In open buffers'
-vim.keymap.set(
-    'n',
-    '<leader><leader>',
-    ':lua Snacks.picker.grep_word()<CR>',
-    opts
-)
-
--- Git
-opts.desc = 'Git status'
-vim.keymap.set('n', '<leader>sG', ':lua Snacks.picker.git_status()<CR>', opts)
-
--- Other
-opts.desc = 'Help'
-vim.keymap.set('n', '<leader>sh', ':lua Snacks.picker.help()<CR>', opts)
-
-opts.desc = 'Keymaps'
-vim.keymap.set('n', '<leader>sk', ':lua Snacks.picker.keymaps()<CR>', opts)
-
-opts.desc = 'Diagnostics'
-vim.keymap.set('n', '<leader>sd', ':lua Snacks.picker.diagnostics()<CR>', opts)
-
-opts.desc = 'Resume'
-vim.keymap.set('n', '<leader>sR', ':lua Snacks.picker.resume()<CR>', opts)
-
-opts.desc = 'Recent'
-vim.keymap.set('n', '<leader>sr', ':lua Snacks.picker.recent()<CR>', opts)
-
-opts.desc = 'Suggestions'
-vim.keymap.set('n', '<leader>ss', ':lua Snacks.picker.spelling()<CR>', opts)
-
-opts.desc = 'Icons'
-vim.keymap.set('n', '<leader>si', ':lua Snacks.picker.icons()<CR>', opts)
-
-opts.desc = 'Projects'
-vim.keymap.set('n', '<leader>sp', ':lua Snacks.picker.projects()<CR>', opts)
-
-opts.desc = 'Colorschemes'
-vim.keymap.set('n', '<leader>sc', ':lua Snacks.picker.colorschemes()<CR>', opts)
-
 return {
     picker_layout = picker_layout,
     spelling_layout = spelling_layout,
-    explorer_layout = explorer_layout,
 }
+
+-- local picker_layout = {
+--     reverse = true,
+--     layout = {
+--         box = 'horizontal',
+--         width = 0.8,
+--         height = 0.9,
+--         border = 'none',
+--         {
+--             box = 'vertical',
+--             border = 'single',
+--             title = ' Results ',
+--             title_pos = 'center',
+--             { win = 'list', border = 'none' },
+--             {
+--                 height = 1,
+--                 win = 'input',
+--                 border = 'top',
+--                 title = '{title} {live} {flags}',
+--                 title_pos = 'center',
+--             },
+--         },
+--         {
+--             win = 'preview',
+--             title = '{preview}',
+--             width = 0.5,
+--             border = 'single',
+--             title_pos = 'center',
+--         },
+--     },
+-- }
