281ecd2220f562dc26f2311b02fcd8fad49793b5
j-zunino <zuninojuanbautista@gmail.com>
2025-07-28 21:34:44 -0300

    feat(nvim): add treesitter-context

--- modules/home/nvim/lua/plugins/treesitter.lua
+++ modules/home/nvim/lua/plugins/treesitter.lua
@@ -25,4 +25,11 @@ return {
         'windwp/nvim-ts-autotag',
         event = { 'BufReadPost', 'BufNewFile' },
     },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        event = { 'BufReadPost', 'BufNewFile' },
+        config = function()
+            require('treesitter-context').setup()
+        end,
+    },
 }
