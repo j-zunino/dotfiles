706d8e5a62c99c48069760f24f3577dfbde2dd93
j-zunino <zuninojuanbautista@gmail.com>
2025-08-15 03:54:23 -0300

    feat(nixos): replace ly with tuigreet
    chore(nixos): remove unused arg

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+    pkgs,
+    inputs,
+    ...
+}: {
     boot = {
         kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
@@ -8,12 +12,23 @@
         };
     };
 
-    services.displayManager.ly = {
+    services.greetd = let
+        tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+        hyprland = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/bin/Hyprland";
+    in {
         enable = true;
-        settings = {
-            save = true;
-            clear_password = true;
-            animation = "matrix";
-        };
+        settings.default_session.command = "${tuigreet} --time --remember --remember-user-session --cmd ${hyprland} --theme 'border=green'";
+    };
+
+    systemd.services.greetd.serviceConfig = {
+        Type = "idle";
+        StandardInput = "tty";
+        StandardOutput = "tty";
+        # without this errors will spam on screen
+        StandardError = "journal";
+        # without these bootlogs will spam on screen
+        TTYReset = true;
+        TTYVHangup = true;
+        TTYVTDisallocate = true;
     };
 }
