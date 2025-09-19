e90587a7620bed2588021a594a2e08d3071908d4
j-zunino <zuninojuanbautista@gmail.com>
2025-09-19 13:59:43 -0300

    refactor(nvim): remove unused which-key configs

--- home/modules/nvim/lua/plugins/which-key.lua
+++ home/modules/nvim/lua/plugins/which-key.lua
@@ -4,7 +4,6 @@ return {
     config = function()
         local wk = require('which-key')
 
-        ---@diagnostic disable-next-line: missing-fields
         wk.setup({
             preset = 'helix',
             delay = 200,
@@ -17,19 +16,11 @@ return {
         wk.add({
             {
                 mode = { 'n' },
-                { '<leader>a', group = 'Avante', icon = { icon = '', color = 'yellow' }, },
                 { '<leader>b', group = 'Buffers', icon = { icon = '', color = 'red' }, },
-                { '<leader>C', group = 'Color', icon = { icon = ' ', color = 'red' }, },
                 { '<leader>c', group = 'Code', icon = { icon = ' ', color = 'red' }, },
-                { '<leader>ct', group = 'Typescript', icon = { icon = '', color = 'cyan' }, },
-                { '<leader>d', group = 'Document', icon = { icon = '󰈙', color = 'cyan' }, },
-                { '<leader>g', group = 'Git', icon = { icon = '', color = 'orange' }, },
-                { '<leader>gh', group = 'Hunks', icon = { icon = '', color = 'orange' }, },
-                { '<leader>l', group = 'Lazygit', icon = { icon = '', color = 'orange' }, },
-                { '<leader>lg', group = 'Lazygit', icon = { icon = '', color = 'orange' }, },
                 { '<leader>r', group = 'Rename', icon = { icon = '' }, },
                 { '<leader>R', group = 'Rest', icon = { icon = '' }, },
-                { '<leader>s', group = 'Snacks picker', icon = { icon = '󱥰', color = 'purple' }, },
+                { '<leader>s', group = 'Pick', icon = { icon = '', color = 'purple' }, },
                 { '<leader>w', group = 'Tailwind', icon = { icon = '󱏿', color = 'cyan' }, },
                 { '<leader>T', group = 'Toggle', },
             },
