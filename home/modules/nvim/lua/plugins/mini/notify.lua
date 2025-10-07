cf3e41bcfa736b8e931b52e328de290267a6415c
j-zunino <zuninojuanbautista@gmail.com>
2025-10-07 16:01:33 -0300

    feat(nvim): move mini.notify to right bottom

--- home/modules/nvim/lua/plugins/mini/notify.lua
+++ home/modules/nvim/lua/plugins/mini/notify.lua
@@ -6,9 +6,25 @@ vim.keymap.set('n', '<leader>n', ':lua MiniNotify.show_history()<CR>', {
     desc = 'Show notification history',
 })
 
+local win_config = function()
+    local has_statusline = vim.o.laststatus > 0
+    local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
+
+    return {
+        anchor = 'SE',
+        col = vim.o.columns,
+        row = vim.o.lines - pad,
+        title = '',
+        border = 'solid',
+    }
+end
+
 return {
     require('mini.notify').setup({
         lsp_progress = { enable = true },
-        window = { winblend = 0 },
+        window = {
+            config = win_config,
+            winblend = 0,
+        },
     }),
 }
