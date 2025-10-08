ba431ca16d88dbf3fab5cd8c5d7f7e77117ec305
j-zunino <zuninojuanbautista@gmail.com>
2025-10-08 15:26:57 -0300

    refactor(nvim): use opts instead of config function

--- home/modules/nvim/lua/plugins/oil.lua
+++ home/modules/nvim/lua/plugins/oil.lua
@@ -1,39 +1,22 @@
 return {
     'stevearc/oil.nvim',
-    config = function()
-        vim.keymap.set(
-            'n',
-            '<C-e>',
-            ':Oil<CR>',
-            { noremap = true, silent = true, desc = 'Open oil' }
-        )
+    opts = {
+        default_file_explorer = true,
+        keymaps = {
+            ['<C-h>'] = 'actions.parent',
+            ['<C-l>'] = 'actions.select',
+            ['<C-r>'] = 'actions.refresh',
+        },
 
-        require('oil').setup({
-            default_file_explorer = true,
-            keymaps = {
-                ['<C-h>'] = 'actions.parent',
-                ['<C-l>'] = 'actions.select',
-                ['<C-r>'] = 'actions.refresh',
-            },
+        win_options = { signcolumn = 'yes' },
+        view_options = { show_hidden = true },
+        confirmation = { border = 'single' },
+    },
 
-            win_options = {
-                signcolumn = 'yes',
-            },
-            view_options = {
-                show_hidden = true,
-            },
-            confirmation = {
-                border = 'single',
-            },
-            progress = {
-                border = 'single',
-            },
-            ssh = {
-                border = 'single',
-            },
-            keymaps_help = {
-                border = 'single',
-            },
-        })
-    end,
+    vim.keymap.set(
+        'n',
+        '<C-e>',
+        ':Oil<CR>',
+        { noremap = true, silent = true, desc = 'Open oil' }
+    ),
 }
