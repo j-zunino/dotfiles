b4fbfee9b676411ae741290641576127447917db
j-zunino <zuninojuanbautista@gmail.com>
2025-10-15 01:55:59 -0300

    feat(nixos): add proton-ge-bin to steam

--- hosts/desktop/modules/gaming.nix
+++ hosts/desktop/modules/gaming.nix
@@ -1,12 +1,15 @@
 {pkgs, ...}: {
     programs = {
         gamemode.enable = true;
-
         steam = {
             enable = true;
             gamescopeSession.enable = true;
             # remotePlay.openFirewall = true;
             # dedicatedServer.openFirewall = true;
+
+            extraCompatPackages = with pkgs; [
+                proton-ge-bin
+            ];
         };
     };
 
