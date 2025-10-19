0b25550fe53bc1471589e7a3dda9cd0845982ee4
j-zunino <zuninojuanbautista@gmail.com>
2025-10-19 01:42:08 -0300

    chore(hyprland): remove comments

--- home/local/desktop/hyprland.nix
+++ home/local/desktop/hyprland.nix
@@ -1,7 +1,6 @@
 {lib, ...}: {
     wayland.windowManager.hyprland = {
         settings = lib.mkAfter {
-            # - [ AUTOSTART ] -
             exec-once = [
                 "hyprctl dispatch workspace 1"
                 "hyprctl dispatch movecursor 960 540"
@@ -10,21 +9,17 @@
                 "[workspace 4 silent] webcord"
             ];
 
-            # - [ MONITORS ] -
             monitor = [
                 "DP-1,1920x1080@60.00,0x0,1"
                 "HDMI-A-1,1920x1080@60.00,-1920x0,1"
                 ", preferred, auto, 1"
             ];
 
-            # - [ WORKSPACES ] -
             workspace = [
                 "1, monitor:DP-1, default:true"
                 "9, monitor:HDMI-A-1, default:true"
             ];
 
-            # - [ INPUTS ] -
-            # TODO: Update
             input = {
                 kb_layout = "latam";
                 kb_model = "latitude";
