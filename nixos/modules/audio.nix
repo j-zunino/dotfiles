5b5aad9aa7cd169808230ca6d36daccec53b2fa2
j-zunino <zuninojuanbautista@gmail.com>
2025-10-19 01:45:25 -0300

    fix(nixos/desktop): monitor audio output

--- nixos/modules/audio.nix
+++ nixos/modules/audio.nix
@@ -1,9 +1,29 @@
-{...}: {
+{
+    pkgs,
+    lib,
+    hostname,
+    ...
+}: {
+    environment.systemPackages = [
+        pkgs.alsa-utils
+    ];
+
     security.rtkit.enable = true;
     services.pipewire = {
         enable = true;
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
+        extraConfig = lib.mkIf (hostname == "desktop") {
+            pipewire."context.modules" = [
+                {
+                    name = "libpipewire-module-alsa-card";
+                    args = {
+                        "card.name" = "alsa_card.pci-0000_01_00.1";
+                        "card.profile" = "output:hdmi-stereo-extra1";
+                    };
+                }
+            ];
+        };
     };
 }
