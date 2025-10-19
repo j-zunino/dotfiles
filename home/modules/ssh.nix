899cc6393ac28fe07899f8e4830faaa11e26467a
j-zunino <zuninojuanbautista@gmail.com>
2025-10-19 01:44:52 -0300

    fix(nixos): update ssh config

--- home/modules/ssh.nix
+++ home/modules/ssh.nix
@@ -3,6 +3,25 @@
 
     programs.ssh = {
         enable = true;
-        addKeysToAgent = "yes";
+        matchBlocks = {
+            "*" = {
+                extraOptions = {
+                    AddKeysToAgent = "yes";
+                    ForwardAgent = "no";
+                    Compression = "no";
+                    ServerAliveInterval = "30";
+                    ServerAliveCountMax = "3";
+                    HashKnownHosts = "yes";
+                    UserKnownHostsFile = "~/.ssh/known_hosts";
+                    ControlMaster = "auto";
+                    ControlPersist = "10m";
+                    ControlPath = "~/.ssh/master-%r@%n:%p";
+                };
+            };
+
+            "github.com" = {
+                extraOptions.ForwardAgent = "yes";
+            };
+        };
     };
 }
