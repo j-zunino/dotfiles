a86f072d6ed98e156b2dffa75c422ef471950596
j-zunino <zuninojuanbautista@gmail.com>
2025-10-22 20:23:30 -0300

    fix(fzf)!: gutter visible after update

--- home/modules/fzf.nix
+++ home/modules/fzf.nix
@@ -8,18 +8,19 @@ in {
         defaultOptions = [
             "--keep-right"
             "--smart-case"
-            "--margin=2"
+            "--margin 2"
             "--info=right"
-            "--scrollbar=█"
-            "--preview-window=hidden:border-sharp"
-            "--preview='bat --style=numbers --color=always --line-range :500 {}'"
+            "--scrollbar █"
+            "--gutter ' '"
+            "--preview-window hidden:border-sharp"
+            "--preview 'bat --style=numbers --color=always --line-range:500 {}'"
 
-            "--color=fg:${colors.gray1-hex},fg+:${colors.fg-hex},bg:-1,bg+:${colors.bg1-hex},gutter:-1"
-            "--color=hl:${colors.bg2-hex},info:${colors.gray1-hex}"
-            "--color=prompt:${colors.accent-hex},spinner:${colors.accent-hex},pointer:${colors.accent-hex}"
-            "--color=border:${colors.gray1-hex},query:${colors.fg-hex}"
+            "--color fg:${colors.gray1-hex},fg+:${colors.fg-hex},bg:-1,bg+:${colors.bg1-hex},gutter:-1"
+            "--color hl:${colors.bg2-hex},info:${colors.gray1-hex}"
+            "--color prompt:${colors.accent-hex},spinner:${colors.accent-hex},pointer:${colors.accent-hex}"
+            "--color border:${colors.gray1-hex},query:${colors.fg-hex}"
 
-            "--bind=alt-p:toggle-preview"
+            "--bind alt-p:toggle-preview"
         ];
     };
 }
