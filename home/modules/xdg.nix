f998cb819f56bfba29cacc7594a723aed4e3e2ef
j-zunino <zuninojuanbautista@gmail.com>
2025-11-12 01:54:55 -0300

    feat(nixos): set default xdg browser

--- home/modules/xdg.nix
+++ home/modules/xdg.nix
@@ -9,6 +9,17 @@
             "audio/flac" = ["mpv.desktop"];
             "audio/ogg" = ["mpv.desktop"];
             "audio/wav" = ["mpv.desktop"];
+
+            "x-scheme-handler/http" = ["zen.desktop"];
+            "x-scheme-handler/https" = ["zen.desktop"];
+            "x-scheme-handler/chrome" = ["zen.desktop"];
+            "text/html" = ["zen.desktop"];
+            "application/x-extension-htm" = ["zen.desktop"];
+            "application/x-extension-html" = ["zen.desktop"];
+            "application/x-extension-shtml" = ["zen.desktop"];
+            "application/xhtml+xml" = ["zen.desktop"];
+            "application/x-extension-xhtml" = ["zen.desktop"];
+            "application/x-extension-xht" = ["zen.desktop"];
         };
     };
 }
