8796fece2e4d9b16d58aae2c586b6a4ca159ab35
j-zunino <zuninojuanbautista@gmail.com>
2025-12-24 03:12:48 -0300

    refactor: merge cli packages in one file

---
+++ home/modules/features/cli/packages.nix
@@ -0,0 +1,92 @@
+{
+    lib,
+    pkgs,
+    config,
+    ...
+}: {
+    config = lib.mkMerge [
+        (lib.mkIf config.features.cli.packages {
+            home.packages = with pkgs; [
+                brightnessctl
+                playerctl
+                nerdfetch
+                libnotify
+                wiremix
+                impala
+                ffmpeg
+                unzip
+                curl
+                wget
+                tree
+                bat
+                zip
+            ];
+        })
+
+        (lib.mkIf config.features.cli.dev-packages {
+            home = {
+                packages = with pkgs; [
+                    # Language Servers
+                    vscode-langservers-extracted
+                    tailwindcss-language-server
+                    typescript-language-server
+                    lua-language-server
+                    lua51Packages.lua
+                    tinymist
+                    emmet-ls
+                    pyright
+                    python3
+                    vtsls
+                    nixd
+
+                    # Formatters, Linters & Compilers
+                    prettierd
+                    alejandra
+                    typstyle
+                    eslint_d
+                    gnumake
+                    stylua
+                    typst
+                    shfmt
+                    gcc
+                    jq
+
+                    # Other
+                    editorconfig-checker
+                    imagemagick
+                    websocat
+                    luarocks
+                    vi-mongo
+                    posting
+                    ripgrep
+                    leetgo
+                    nodejs
+                    cargo
+                    pnpm
+                    # love
+                    vale
+                    nmap
+                    fd
+                ];
+
+                file."dotfiles/alejandra.toml".text = ''indentation = "FourSpaces"'';
+
+                file.".editorconfig".text = ''
+                    root = true
+
+                    [*]
+                    indent_style = space
+                    indent_size = 4
+                    tab_width = 4
+                    end_of_line = lf
+                    charset = utf-8
+                    trim_trailing_whitespace = true
+                    insert_final_newline = true
+                    max_line_length = 80
+                    quote_type = single
+                    spaces_around_brackets = inside
+                '';
+            };
+        })
+    ];
+}
