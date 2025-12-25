de2d4a12dab057dcb1dca2e8399905297247fa99
j-zunino <zuninojuanbautista@gmail.com>
2025-12-25 17:39:25 -0300

    fix: move spicetify input to parent scope

--- home/modules/features/gui/spotify.nix
+++ home/modules/features/gui/spotify.nix
@@ -4,22 +4,21 @@
     inputs,
     config,
     ...
-}: {
-    config = let
-        spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-    in
-        lib.mkIf config.features.gui.spotify {
-            imports = [inputs.spicetify-nix.homeManagerModules.default];
+}: let
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+in {
+    imports = [inputs.spicetify-nix.homeManagerModules.default];
 
-            programs.spicetify = {
-                enable = true;
-                enabledExtensions = with spicePkgs.extensions; [
-                    # adblock
-                    hidePodcasts
-                ];
+    config = lib.mkIf config.features.gui.spotify {
+        programs.spicetify = {
+            enable = true;
+            enabledExtensions = with spicePkgs.extensions; [
+                # adblock
+                hidePodcasts
+            ];
 
-                theme = spicePkgs.themes.text;
-                colorScheme = "EverforestDarkMedium";
-            };
+            theme = spicePkgs.themes.text;
+            colorScheme = "EverforestDarkMedium";
         };
+    };
 }
