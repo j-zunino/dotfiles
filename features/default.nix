eb53602c44133d53d582d3291009cfcb11fbe4ab
j-zunino <zuninojuanbautista@gmail.com>
2025-12-28 04:54:07 -0300

    feat: update features descriptions

--- features/default.nix
+++ features/default.nix
@@ -1,14 +1,16 @@
 {lib, ...}: {
     options.features = {
         gui = {
-            launcher = lib.mkEnableOption "Rofi";
-            multimedia = lib.mkEnableOption "Multimedia";
+            launcher = lib.mkEnableOption "App launcher";
+            multimedia = lib.mkEnableOption "Multimedia support";
             notifications = lib.mkEnableOption "Notification daemon";
-            packages = lib.mkEnableOption "Packages that doesn't need configuration";
-            spotify = lib.mkEnableOption "Spotify";
+            packages = lib.mkEnableOption "GUI packages that do not require extra configuration";
+            spotify = lib.mkEnableOption "Spotify client";
+
             terminal = {
-                wezterm = lib.mkEnableOption "Wezterm";
+                wezterm = lib.mkEnableOption "WezTerm terminal emulator";
             };
+
             browser = {
                 zen = lib.mkEnableOption "Zen browser";
                 brave = lib.mkEnableOption "Brave browser";
@@ -16,46 +18,46 @@
         };
 
         cli = {
-            aichat = lib.mkEnableOption "AiChat";
-            btop = lib.mkEnableOption "Btop";
-            bun = lib.mkEnableOption "JS runtime, bundler, transpiler and package manager";
-            delta = lib.mkEnableOption "Git pager";
-            devPackages = lib.mkEnableOption "LSPs, formatters, linters and other";
+            aichat = lib.mkEnableOption "AI chat";
+            btop = lib.mkEnableOption "Btop system monitor";
+            bun = lib.mkEnableOption "Bun JS runtime, bundler, and package manager";
+            delta = lib.mkEnableOption "Git diff pager";
+            devPackages = lib.mkEnableOption "Developer tools";
             fzf = lib.mkEnableOption "Fuzzy finder";
-            git = lib.mkEnableOption "Git";
-            lazygit = lib.mkEnableOption "Lazygit";
-            nvim = lib.mkEnableOption "Neovim";
-            packages = lib.mkEnableOption "Packages that doesn't need configuration";
+            git = lib.mkEnableOption "Git version control system";
+            lazygit = lib.mkEnableOption "Git TUI client";
+            nvim = lib.mkEnableOption "Text editor";
+            packages = lib.mkEnableOption "CLI packages that do not require configuration";
             ssh = lib.mkEnableOption "SSH";
             tmux = lib.mkEnableOption "Terminal multiplexer";
-            zsh = lib.mkEnableOption "Zsh shell";
+            zsh = lib.mkEnableOption "Shell";
         };
 
         wm = {
-            hyprland = lib.mkEnableOption "Hyprland window manager";
-            waybar = lib.mkEnableOption "Waybar";
+            hyprland = lib.mkEnableOption "Wayland window manager";
+            waybar = lib.mkEnableOption "Status bar";
         };
 
         gaming = {
-            emulation = lib.mkEnableOption "Emulation";
-            heroic = lib.mkEnableOption "Epic games game launcher";
+            emulation = lib.mkEnableOption "Console emulation";
+            heroic = lib.mkEnableOption "Heroic Games Launcher";
             minecraft = lib.mkEnableOption "Minecraft launcher";
-            openFirewall = lib.mkEnableOption "";
-            packages = lib.mkEnableOption "";
-            proton = lib.mkEnableOption "";
-            steam = lib.mkEnableOption "";
-            tweaks = lib.mkEnableOption "Settings necessary for max performance";
+            openFirewall = lib.mkEnableOption "Open firewall ports (steam)";
+            packages = lib.mkEnableOption "Gaming-related packages";
+            proton = lib.mkEnableOption "Proton compatibility tools";
+            steam = lib.mkEnableOption "Steam";
+            tweaks = lib.mkEnableOption "System tweaks for maximum gaming performance";
         };
 
         hardware = {
-            audio = lib.mkEnableOption "";
-            bluetooth = lib.mkEnableOption "";
-            driveMounting = lib.mkEnableOption "";
-            dualboot = lib.mkEnableOption "";
-            ethernet = lib.mkEnableOption "";
-            greetd = lib.mkEnableOption "";
-            nvidia = lib.mkEnableOption "";
-            wifi = lib.mkEnableOption "";
+            audio = lib.mkEnableOption "Audio support";
+            bluetooth = lib.mkEnableOption "Bluetooth support";
+            driveMounting = lib.mkEnableOption "Automatic mounting of removable drives";
+            dualboot = lib.mkEnableOption "Dual-boot configuration";
+            ethernet = lib.mkEnableOption "Wired network support";
+            greetd = lib.mkEnableOption "Greetd";
+            nvidia = lib.mkEnableOption "NVIDIA GPU support";
+            wifi = lib.mkEnableOption "Wireless network support";
         };
     };
 }
