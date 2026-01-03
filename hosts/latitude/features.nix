dd51f4ad1a9f1ab090bdcf22c8f199ecc9cdc30c
j-zunino <zuninojuanbautista@gmail.com>
2026-01-03 00:18:37 -0300

    style: format

--- hosts/latitude/features.nix
+++ hosts/latitude/features.nix
@@ -1,61 +1,61 @@
 {...}: {
-  features = {
-    gui = {
-      launcher = true;
-      multimedia = true;
-      notifications = true;
-      packages = true;
-      spotify = true;
-      terminal = {
-        wezterm = true;
-      };
-      browser = {
-        zen = true;
-        brave = true;
-      };
-    };
+    features = {
+        gui = {
+            launcher = true;
+            multimedia = true;
+            notifications = true;
+            packages = true;
+            spotify = true;
+            terminal = {
+                wezterm = true;
+            };
+            browser = {
+                zen = true;
+                brave = true;
+            };
+        };
 
-    cli = {
-      aichat = true;
-      btop = true;
-      bun = false;
-      delta = true;
-      devPackages = true;
-      fzf = true;
-      git = true;
-      lazygit = true;
-      nvim = true;
-      packages = true;
-      ssh = true;
-      tmux = true;
-      zsh = true;
-    };
+        cli = {
+            aichat = true;
+            btop = true;
+            bun = false;
+            delta = true;
+            devPackages = true;
+            fzf = true;
+            git = true;
+            lazygit = true;
+            nvim = true;
+            packages = true;
+            ssh = true;
+            tmux = true;
+            zsh = true;
+        };
 
-    wm = {
-      hyprland = true;
-      waybar = true;
-    };
+        wm = {
+            hyprland = true;
+            waybar = true;
+        };
 
-    gaming = {
-      emulation = false;
-      heroic = false;
-      minecraft = false;
-      openFirewall = false;
-      packages = false;
-      proton = false;
-      steam = false;
-      tweaks = false;
-    };
+        gaming = {
+            emulation = false;
+            heroic = false;
+            minecraft = false;
+            openFirewall = false;
+            packages = false;
+            proton = false;
+            steam = false;
+            tweaks = false;
+        };
 
-    hardware = {
-      audio = true;
-      bluetooth = true;
-      driveMounting = true;
-      dualboot = false;
-      ethernet = false;
-      greetd = true;
-      nvidia = false;
-      wifi = true;
+        hardware = {
+            audio = true;
+            bluetooth = true;
+            driveMounting = true;
+            dualboot = false;
+            ethernet = false;
+            greetd = true;
+            nvidia = false;
+            wifi = true;
+        };
     };
-  };
 }
