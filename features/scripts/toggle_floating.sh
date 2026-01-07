f35773fa3bc16039491acc041054bd6f1b506f46
j-zunino <zuninojuanbautista@gmail.com>
2026-01-06 21:42:33 -0300

    feat: support foot terminal in toggle_floating

--- features/scripts/toggle_floating.sh
+++ features/scripts/toggle_floating.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
-CLASS="$1"
-APP="$2"
+TERM="$1"
+CLASS="$2"
+APP="$3"
 
 if hyprctl clients | grep -q "class: $CLASS"; then
     hyprctl dispatch closewindow "class:$CLASS"
-else
-    exec wezterm start --class "$CLASS" "$APP"
+    exit 0
 fi
+
+case "$TERM" in
+    wezterm)
+        exec wezterm start --class "$CLASS" "$APP"
+        ;;
+    foot)
+        exec foot --app-id="$CLASS" "$APP"
+        ;;
+    *)
+        echo "Unknown terminal: $TERM" >&2
+        exit 1
+        ;;
+esac
