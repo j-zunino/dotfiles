cd0252f92fdd7babefd94fb2bf8c80cc308ca7b5
j-zunino <zuninojuanbautista@gmail.com>
2026-01-06 21:45:32 -0300

    feat: start webcord minimized

--- features/wm/home/hypr/autostart.nix
+++ features/wm/home/hypr/autostart.nix
@@ -5,17 +5,16 @@
 }: {
     wayland.windowManager.hyprland.settings.exec-once =
         [
-            "sleep 4"
+            "sleep 5"
             "wpaperd"
             "waybar"
             "hypridle"
             "wl-paste --watch cliphist store"
         ]
         ++ lib.optionals (hostname == "desktop") [
-            "steam -silent"
             "[workspace 4 silent] spotify"
-            "[workspace 4 silent] webcord"
-            "sleep 2"
+            "webcord -m"
+            "steam -silent"
             "hyprctl dispatch workspace 1"
         ]
         ++ lib.optionals (hostname == "latitude") [
