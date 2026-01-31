fef10a25c8f22e508b62b64a0116da2147c944a3
j-zunino <zuninojuanbautista@gmail.com>
2026-01-31 01:27:14 -0300

    feat: add mangowc desktop monitor rules

--- features/wm/home/mango/mango.nix
+++ features/wm/home/mango/mango.nix
@@ -2,6 +2,7 @@
     lib,
     config,
     inputs,
+    hostname,
     ...
 }: {
     imports = [
@@ -14,23 +15,29 @@
     config = lib.mkIf config.features.wm.mango {
         wayland.windowManager.mango = {
             enable = true;
-            settings = ''
+            settings = lib.mkMerge [
+                ''
+                    new_is_master=0
+                    circle_layout=tile,scroller,vertical_scroller
 
-                new_is_master=0
-                circle_layout=tile,scroller,vertical_scroller
+                    repeat_rate=40
+                    repeat_delay=300
+                    xkb_rules_layout=latam
+                    xkb_rules_options=caps:escape
 
-                repeat_rate=40
-                repeat_delay=300
-                xkb_rules_layout=latam
-                xkb_rules_options=caps:escape
+                    allow_tearing=2
+                    accel_profile=0
+                    warpcursor=0
+                    drag_corner=4
+                    drag_warp_cursor=0
+                    drag_tile_to_tile=1
+                ''
 
-                allow_tearing=2
-                accel_profile=0
-                warpcursor=0
-                drag_corner=4
-                drag_warp_cursor=0
-                drag_tile_to_tile=1
-            '';
+                (lib.mkIf (hostname == "desktop") ''
+                    monitorrule=name:HDMI-A-1,width:1360,height:768,refresh:60,x:0,y:0,scale:1
+                    monitorrule=name:DP-1,width:1920,height:1080,refresh:60,x:1360,y:0,scale:1
+                '')
+            ];
         };
     };
 }
