1bdec794c6ef470669a5571f37d7f1857ff97cf6
j-zunino <zuninojuanbautista@gmail.com>
2026-02-25 00:23:32 -0300

    feat: change mango look

--- features/wm/home/mango/look.nix
+++ features/wm/home/mango/look.nix
@@ -3,15 +3,15 @@
         colors = config.my.colors;
     in ''
         shadow_only_floating=0
-        border_radius=0
-        gappih=0
-        gappiv=0
-        gappoh=0
-        gappov=0
-        borderpx=2
         animations=0
+        unfocused_opacity=0.95
+        border_radius=0
+        gappih=4
+        gappiv=4
+        gappoh=4
+        gappov=4
+        borderpx=0
         cursor_size=${toString config.stylix.cursor.size}
-
         rootcolor=0x${colors.bg1.hexNoPrefix}ff
         bordercolor=0x${colors.bg1.hexNoPrefix}ff
         focuscolor=0x${colors.accent.hexNoPrefix}ff
