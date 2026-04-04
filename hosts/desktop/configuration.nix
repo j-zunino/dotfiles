c2b448d5da88a3117e97029713e3d54613eb1ee7
j-zunino <zuninojuanbautista@gmail.com>
2026-04-03 23:37:49 -0300

    feat: remove desktop toshiba mount config

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,9 +1,4 @@
 {...}: {
-    fileSystems."/mnt/toshiba" = {
-        device = "/dev/disk/by-uuid/2b4cd237-785d-4eb9-bdef-9fb799c991cb";
-        fsType = "ext4";
-    };
-
     fileSystems."/mnt/wd" = {
         device = "/dev/disk/by-uuid/8062E24362E23E12";
         fsType = "ntfs";
