c465a4d3d497e4ea7651f53308b7b799eb9947cf
j-zunino <zuninojuanbautista@gmail.com>
2026-04-03 23:38:08 -0300

    feat: update desktop wd mount config

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,7 +1,6 @@
 {...}: {
     fileSystems."/mnt/wd" = {
-        device = "/dev/disk/by-uuid/8062E24362E23E12";
-        fsType = "ntfs";
-        options = ["rw" "uid=1000" "gid=1000" "umask=0022"];
+        device = "/dev/disk/by-uuid/51e2c8ae-eb73-4ba7-b436-aaec4307e1dd";
+        fsType = "ext4";
     };
 }
