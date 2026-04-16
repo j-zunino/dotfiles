6311fec502c040e6ea2b51050fdf459f24b41485
j-zunino <zuninojuanbautista@gmail.com>
2026-04-16 00:57:22 -0300

    feat: use vim.hl instead of deprecated vim.highlight

--- features/cli/nvim/lua/core/autocmds.lua
+++ features/cli/nvim/lua/core/autocmds.lua
@@ -6,7 +6,7 @@ api.nvim_create_autocmd('BufEnter', { command = [[set formatoptions-=cro]] })
 -- Highlight on yank
 api.nvim_create_autocmd('TextYankPost', {
     callback = function()
-        vim.highlight.on_yank()
+        vim.hl.on_yank()
     end,
 })
 
