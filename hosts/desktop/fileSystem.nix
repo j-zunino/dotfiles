b99e2db864145c742c0a57b2dc618f0f5326f880
j-zunino <zuninojuanbautista@gmail.com>
2026-07-04 19:25:06 -0300

    feat: allow desktop fileSystem to show in gvfs

--- hosts/desktop/fileSystem.nix
+++ hosts/desktop/fileSystem.nix
@@ -3,12 +3,25 @@
         fileSystems."/mnt/games-win" = {
             device = "/dev/disk/by-uuid/7C78EE9F78EE5782";
             fsType = "ntfs";
-            options = ["defaults" "uid=1000" "gid=100" "dmask=022" "fmask=133"];
+            options = [
+                "defaults"
+                "nofail"
+                "x-gvfs-show"
+                "uid=1000"
+                "gid=100"
+                "dmask=022"
+                "fmask=133"
+            ];
         };
 
         fileSystems."/mnt/wd" = {
             device = "/dev/disk/by-uuid/51e2c8ae-eb73-4ba7-b436-aaec4307e1dd";
             fsType = "ext4";
+            options = [
+                "defaults"
+                "nofail"
+                "x-gvfs-show"
+            ];
         };
     };
 }
