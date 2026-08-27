23bc2ea5a255a4b115dbbfb90ddbc05a67a7575e
j-zunino <zuninojuanbautista@gmail.com>
2026-08-27 13:40:39 -0300

    feat: enable gnome-keyring in pam

--- modules/wm/keyring.nix
+++ modules/wm/keyring.nix
@@ -1,6 +1,7 @@
 {...}: {
     flake.modules.nixos.keyring = {...}: {
         services.gnome.gnome-keyring.enable = true;
+        security.pam.services.login.enableGnomeKeyring = true;
     };
 
     flake.modules.homeManager.keyring = {pkgs, ...}: {
